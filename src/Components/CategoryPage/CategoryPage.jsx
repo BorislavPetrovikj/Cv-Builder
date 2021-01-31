6e49be4d9a238aa51ba9206a5d17c2aa91203858
Borko <petrovicborko392@gmail.com>
2021-01-31 20:02:41 +0100

    Responsive

--- src/Components/CategoryPage/CategoryPage.jsx
+++ src/Components/CategoryPage/CategoryPage.jsx
@@ -6,57 +6,57 @@ import "./CategoryPage.css";
 const CategoryPage = (props) => {
   return (
     <div className="CategoryPage bg">
-      <div className="row">
+      <div className="row marginRow">
         <div className="col-12 text-left p-4">
           <h1>Choose your Category</h1>
         </div>
-        <div class="col-sm-3">
-          <div class="card p-5">
+        <div class="col">
+          <div class="card p-4">
             <div class="card-body  p-5">
               <h3 class="card-title">Web development</h3>
             </div>
-            <Link to="/resumes">
+            <Link class="text-right" to="/resumes">
               {" "}
-              <button type="button" class="btn btn-primary text-right">
+              <button type="button" class="btn btn-primary ">
                 Choose
               </button>
             </Link>{" "}
           </div>
         </div>
-        <div class="col-sm-3">
-          <div class="card p-5">
+        <div class="col">
+          <div class="card p-4">
             <div class="card-body  p-5">
-              <h3 class="card-title">Data Sience</h3>
+              <h3 class="card-title">Web development</h3>
             </div>
-            <Link to="/resumes">
+            <Link class="text-right" to="/resumes">
               {" "}
-              <button type="button" class="btn btn-primary">
+              <button type="button" class="btn btn-primary ">
                 Choose
               </button>
             </Link>{" "}
           </div>
         </div>
-        <div class="col-sm-3">
-          <div class="card p-5 ">
-            <div class="card-body  p-5 ">
-              <h3 class="card-title">Digital Marketing</h3>
-            </div>
-            <Link to="/resumes">
-              {" "}
-              <button type="button" class="btn btn-primary">
-                Choose
-              </button>
-            </Link>{" "}
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="card p-5">
+        <div class="col">
+          <div class="card p-4">
             <div class="card-body  p-5">
-              <h3 class="card-title">Design</h3>
+              <h3 class="card-title">Web development</h3>
             </div>
-            <Link to="/resumes">
+            <Link class="text-right" to="/resumes">
               {" "}
-              <button type="button" class="btn btn-primary">
+              <button type="button" class="btn btn-primary ">
+                Choose
+              </button>
+            </Link>{" "}
+          </div>
+        </div>
+        <div class="col">
+          <div class="card p-4">
+            <div class="card-body  p-5">
+              <h3 class="card-title">Web development</h3>
+            </div>
+            <Link class="text-right" to="/resumes">
+              {" "}
+              <button type="button" class="btn btn-primary ">
                 Choose
               </button>
             </Link>{" "}
