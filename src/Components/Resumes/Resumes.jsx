42eeca81502134d2b58a9afefe1cc27249873517
Borko <petrovicborko392@gmail.com>
2021-02-01 13:13:00 +0100

    Routing

--- src/Components/Resumes/Resumes.jsx
+++ src/Components/Resumes/Resumes.jsx
@@ -1,38 +1,68 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Banner from "../Banner/Banner";
 import "./Resumes.css";
-class Resumes extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Cv from "../CV/Cv";
+import Linkedin from "../Linkedin/Linkedin";
+import Wearelaika from "../Wearelaika/Wearelaika";
+import Banner from "../Banner/Banner";
 
-  render() {
-    return (
-      <div className="Resumes bg">
-        <Link to="/cv">
-          {" "}
-          <button type="button" class="btn btn-primary">
-            CV
-          </button>
-        </Link>
-        <Link to="/linkedin">
-          {" "}
-          <button type="button" class="btn btn-primary">
-            Linkedin
-          </button>
-        </Link>
-        <Link to="/wearelaika">
-          {" "}
-          <button type="button" class="btn btn-primary">
-            WeareLaika
-          </button>
-        </Link>
-        <Banner />
+const routes = [
+  {
+    path: "/CV",
+    exact: true,
+    main: () => <Cv />,
+  },
+  {
+    path: "/Linkedin",
+    main: () => <Linkedin />,
+  },
+  {
+    path: "/Wearelaika",
+    main: () => <Wearelaika />,
+  },
+];
+
+export default function Resumes() {
+  return (
+    <Router>
+      <div>
+        <div>
+          <Link to="/CV">
+            {" "}
+            <button type="button" class="btn btn-primary mt-4">
+              CV
+            </button>
+          </Link>
+
+          <Link to="/Linkedin">
+            {" "}
+            <button type="button" class="btn btn-primary mt-4">
+              Linkedin
+            </button>
+          </Link>
+
+          <Link to="/Wearelaika">
+            {" "}
+            <button type="button" class="btn btn-primary mt-4">
+              Wearelaika
+            </button>
+          </Link>
+        </div>
+
+        <div>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                children={<route.main />}
+              />
+            ))}
+          </Switch>
+        </div>
       </div>
-    );
-  }
+      <Banner />
+    </Router>
+  );
 }
-
-export default Resumes;
